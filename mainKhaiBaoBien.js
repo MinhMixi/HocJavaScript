19485c1d5c882abcaed77aac1d662975453b064a
MinhMixi <ngotuanminh05082002@gmail.com>
2024-03-31 00:36:06 +0700

    done

---
+++ mainKhaiBaoBien.js
@@ -0,0 +1,12 @@
+// // Khai báo biến
+// var fullName = 'Ngo Tuan Minh';
+// var age = 22; // Khai báo tuổi
+// // Gọi hàm thông báo
+// alert(fullName);
+// alert(age);
+
+
+
+
+
+
---
+++ mainToanTu.js
@@ -0,0 +1,22 @@
+// // Toán tử số học
+// var a = 1 + 2
+// console.log(a)
+
+// // Toán tử gán (=)
+// var fullName = 'Ngo Tuan Minh'
+
+// // Toán tử so sánh
+// var a = 1
+// var b = 2
+// if(a<b){
+//     alert('true')
+// }else{
+//     alert('false')
+// }
+
+// // Toán tử logic
+// var a = 1
+// var b = 2
+// if(a > 0 && b > 0){
+//     alert('a và b > 0')
+// }
